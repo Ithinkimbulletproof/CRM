6f0be0e558c7541de584c09bfa6757622d4866c7
Ithinkimbulletproof <devilmaker228@gmail.com>
2024-11-23 23:42:49 +0500

    Create Apps

--- crm/settings.py
+++ crm/settings.py
@@ -11,6 +11,10 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    "chat",
+    "core",
+    "employees",
+    "tasks",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

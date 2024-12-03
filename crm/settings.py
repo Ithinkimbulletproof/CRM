4d8a9bc92d14a0b07d9550dce41251a151dc5b56
Ithinkimbulletproof <devilmaker228@gmail.com>
2024-12-03 15:58:42 +0500

    Add Docker

--- crm/settings.py
+++ crm/settings.py
@@ -105,6 +105,17 @@ CHANNEL_LAYERS = {
     },
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
